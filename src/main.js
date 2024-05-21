9b2d5507e5eec52f14fa48667b1c9359ff2408ca
Jordan Haddadi <jordanhaddadi@Jordans-MacBook-Pro.local>
2024-05-21 16:00:50 -0600

    Lite refactor for router

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PageHome from '@/components/PageHome.vue'
-import * as VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     { 
         path: '/',
+        name: 'Home',
         component: PageHome
     }
 ]
 
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
+const router = createRouter({
+    history: createWebHistory(),
     routes,
 })
 
