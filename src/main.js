432c7d8ed0c016b251936f91e83e9a44ba55e7b7
Jordan Haddadi <jordanhaddadi@Jordans-MacBook-Pro.local>
2024-05-21 15:58:24 -0600

    Adding Vue Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from '@/components/PageHome.vue'
+import * as VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+    { 
+        path: '/',
+        component: PageHome
+    }
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
