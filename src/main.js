2cdce146e6a924a1b27975d1cf724ac8c2d12687
Jordan Haddadi <jordanhaddadi@Jordans-MacBook-Pro.local>
2024-05-22 10:41:10 -0600

    Dynamic routing using Vue Router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PageHome from '@/components/PageHome.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import PageThreadShow from '@/components/PageThreadShow.vue'
+import { createRouter, createWebHistory } from 'vue-router' 
 
 const routes = [
     { 
         path: '/',
         name: 'Home',
         component: PageHome
+    },
+    { 
+        path: '/thread/:id',
+        name: 'ThreadShow',
+        component: PageThreadShow
     }
 ]
 
